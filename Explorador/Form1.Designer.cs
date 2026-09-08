0e180e14e048b5a47c79ee78de3b092fb226d323
MeredithDaniela <mcerda0@ucol.mx>
2026-09-08 08:24:27 -0600

    Avance

--- Explorador/Form1.Designer.cs
+++ Explorador/Form1.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            Combo = new ComboBox();
+            picbox = new PictureBox();
+            label1 = new Label();
+            link = new LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)picbox).BeginInit();
+            SuspendLayout();
+            // 
+            // Combo
+            // 
+            Combo.FormattingEnabled = true;
+            Combo.Location = new Point(12, 32);
+            Combo.Name = "Combo";
+            Combo.Size = new Size(577, 23);
+            Combo.TabIndex = 0;
+            Combo.SelectedIndexChanged += Combo_SelectedIndexChanged;
+            // 
+            // picbox
+            // 
+            picbox.Location = new Point(12, 81);
+            picbox.Name = "picbox";
+            picbox.Size = new Size(279, 305);
+            picbox.TabIndex = 1;
+            picbox.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(321, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 19);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // link
+            // 
+            link.AutoSize = true;
+            link.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            link.Location = new Point(269, 406);
+            link.Name = "link";
+            link.Size = new Size(72, 19);
+            link.TabIndex = 3;
+            link.TabStop = true;
+            link.Text = "linkLabel1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(601, 450);
+            Controls.Add(link);
+            Controls.Add(label1);
+            Controls.Add(picbox);
+            Controls.Add(Combo);
+            Name = "Form1";
+            Text = "Selector de funciones";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)picbox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox Combo;
+        private PictureBox picbox;
+        private Label label1;
+        private LinkLabel link;
     }
 }
--- Explorador/Form1.cs
+++ Explorador/Form1.cs
@@ -2,9 +2,79 @@ namespace Explorador
 {
     public partial class Form1 : Form
     {
+        //Ruta donde se encuentra las imagenes
+        private readonly string rutaRecursos =
+        Path.Combine(Application.StartupPath, "Imagenes");
+
         public Form1()
         {
             InitializeComponent();
+            //Cargar las distribuciones al iniciar
+            CargarDistribuciones();
+
+            //Configurae wl picture box
+            picbox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //===========================================
+        //Cargar distribuciones
+        //===========================================
+        private void CargarDistribuciones()
+        {
+            Combo.Items.Clear();
+
+            Combo.Items.Add("Puppy Linux");
+            Combo.Items.Add("Slax");
+            Combo.Items.Add("antiX");
+            Combo.Items.Add("Debian");
+            Combo.Items.Add("Tiny Core Linux");
+
+            //NO selccionar ningun item al inicio
+            Combo.SelectedIndex = -1;
+        }
+
+        private void Combo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //revisar que el combo box tenga info
+            if (Combo.SelectedIndex == -1) 
+            {
+                return;
+            }
+            string distroSeleccionada =
+                Combo.SelectedItem.ToString();
+            switch (distroSeleccionada)
+            {
+                case "Puppy Linux":
+                    MostrarDistro(
+                        "Puppy Linux",
+                        "Puppy Linux es una distribucion ligera" +
+                        "diseñada para funcionar correctamente en" +
+                        "equipos de pocos recursos.\r\n\r\n" +
+                        "Requisitos aproximados:\r\n" +
+                        "· Procesador X86 o compatible\r\n" +
+                        "· 1 Giga de RAM recomendado\r\n" +
+                        "· Espacio reducido en diso",
+                        "puppy.png",
+                        "https://puppylinux-woof-ce.github.io/");
+                    break;
+                case "Slax":
+                    MostrarDistro(
+                        "Slax",
+                        "Slax es una distribucion pequeña y" +
+                        "portale que puede ejecutarse desde una" +
+                        "memoria USB\r\n\r\n" +
+                        "Requisitos aproximados:\r\n" +
+                        "· Procesador X86_64 o compatible\r\n" +
+                        "· 512 Mega de RAM recomendado\r\n" +
+                        "· Espacio reducido en diso",
+                        "puppy.png",
+                        "https://puppylinux-woof-ce.github.io/"
+                        );
+            }
         }
     }
 }
